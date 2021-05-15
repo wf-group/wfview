96a4ff1e225a78bc562b5041c172c19c51e7aea1
Phil Taylor <phil@m0vse.uk>
2021-05-15 10:42:44 +0100

    Small fixes for rigctld

--- rigctld.h
+++ rigctld.h
@@ -8,8 +8,40 @@
 #include <QSet>
 #include <QDataStream>
 
+#include <map>
+#include <vector>
+#include <typeindex>
+
 #include "rigcommander.h"
 
+typedef void (*voidFunction)(void);
+struct Interface {
+
+    std::map<std::string, std::pair<voidFunction, std::type_index>> m1;
+
+    template<typename T>
+    void insert(std::string s1, std::string s2, T f1) {
+        auto tt = std::type_index(typeid(f1));
+        m1.insert(std::make_pair(s1,
+            std::make_pair((voidFunction)f1, tt)));
+        m1.insert(std::make_pair(s2,
+            std::make_pair((voidFunction)f1, tt)));
+    }
+
+    template<typename T, typename... Args>
+    T searchAndCall(std::string s1, Args&&... args) {
+        auto mapIter = m1.find(s1);
+        /*chk if not end*/
+        auto mapVal = mapIter->second;
+
+        // auto typeCastedFun = reinterpret_cast<T(*)(Args ...)>(mapVal.first); 
+        auto typeCastedFun = (T(*)(Args ...))(mapVal.first);
+
+        //compare the types is equal or not
+        assert(mapVal.second == std::type_index(typeid(typeCastedFun)));
+        return typeCastedFun(std::forward<Args>(args)...);
+    }
+};
 
 class rigCtlD : public QTcpServer
 {
@@ -71,6 +103,9 @@ private:
     QString getMode(unsigned char mode, bool datamode);
     unsigned char getMode(QString modeString);
     QString getFilter(unsigned char mode, unsigned char filter);
+
+    Interface commands;
 };
+
 
 #endif
