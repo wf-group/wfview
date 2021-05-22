c344ecc9281f39f4e25c4a1f730fa783b9510f57
Phil Taylor <phil@m0vse.uk>
2021-05-22 09:24:19 +0100

    Try increasing audio device buffer size

--- audiohandler.cpp
+++ audiohandler.cpp
@@ -902,11 +902,11 @@ void audioHandler::reinit()
         audioOutput = new QAudioOutput(deviceInfo, format, this);
 
 		// This seems to only be needed on Linux but is critical in aligning buffer sizes.
-#ifdef Q_OS_MAC
+//#ifdef Q_OS_MAC
         audioOutput->setBufferSize(chunkSize*8);
-#else
-        audioOutput->setBufferSize(chunkSize*4);
-#endif
+//#else
+//        audioOutput->setBufferSize(chunkSize*4);
+//#endif
 
 		connect(audioOutput, SIGNAL(notify()), SLOT(notified()));
         connect(audioOutput, SIGNAL(stateChanged(QAudio::State)), SLOT(stateChanged(QAudio::State)));
