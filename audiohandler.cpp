2a61e7f849a39f4c847fd311a6a91fc1a0bee48e
Phil Taylor <phil@m0vse.uk>
2021-05-27 18:48:19 +0100

    Latency reporting causing issue?

--- audiohandler.cpp
+++ audiohandler.cpp
@@ -180,9 +180,9 @@ int audioHandler::readData(void* outputBuffer, void* inputBuffer, unsigned int n
 			}
 
 			if (currentLatency > (int)audioLatency) {
-				qInfo(logAudio()) << (isInput ? "Input" : "Output") << "Packet " << hex << packet.seq <<
-					" arrived too late (increase output latency!) " <<
-					dec << packet.time.msecsTo(QTime::currentTime()) << "ms";
+				//qInfo(logAudio()) << (isInput ? "Input" : "Output") << "Packet " << hex << packet.seq <<
+				//	" arrived too late (increase output latency!) " <<
+				//	dec << packet.time.msecsTo(QTime::currentTime()) << "ms";
 				//lastSeq = packet.seq;
 				//if (!ringBuf->try_read(packet))
 				//	return sentlen;
