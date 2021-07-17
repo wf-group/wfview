5c58580514626d83af66ac59c6f3e1352ea3c6f0
Phil Taylor <phil@m0vse.uk>
2021-07-17 12:07:52 +0100

    Replace function wrongly deleted by merge

--- wfmain.h
+++ wfmain.h
@@ -743,6 +743,7 @@ private:
 
     void initPeriodicCommands();
     void insertPeriodicCommand(cmds cmd, unsigned char priority);
+    void insertSlowPeriodicCommand(cmds cmd, unsigned char priority);
 
     void calculateTimingParameters();
 
