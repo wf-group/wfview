a40e5bf2c92afb09a83fd19ad9e6491679a646c8
Phil Taylor <phil@m0vse.uk>
2022-04-11 11:49:48 +0100

    Update audiohandler.cpp

--- audiohandler.cpp
+++ audiohandler.cpp
@@ -407,7 +407,7 @@ void audioHandler::getNextAudioChunk()
 	livePacket.time= QTime::currentTime();
 	livePacket.sent = 0;
 	memcpy(&livePacket.guid, setup.guid, GUIDLEN);
-	while (tempBuf.data.length() > 0) {
+	while (tempBuf.data.length() > format.bytesForDuration(setup.blockSize * 1000)) {
 		livePacket.data = tempBuf.data.mid(0, format.bytesForDuration(setup.blockSize * 1000));
 		tempBuf.data.remove(0, format.bytesForDuration(setup.blockSize * 1000));
 		if (livePacket.data.length() > 0)
