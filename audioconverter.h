77576d22f4d9dec4e32d4264c9cea82adb8e07de
Phil Taylor <phil@m0vse.uk>
2022-05-08 11:46:33 +0100

    Create function to convert Icom format to QAudioFormat

--- audioconverter.h
+++ audioconverter.h
@@ -65,4 +65,51 @@ typedef Eigen::Matrix<qint8, Eigen::Dynamic, 1> VectorXint8;
 typedef Eigen::Matrix<qint16, Eigen::Dynamic, 1> VectorXint16;
 typedef Eigen::Matrix<qint32, Eigen::Dynamic, 1> VectorXint32;
 
+static QAudioFormat toQAudioFormat(quint8 codec, quint32 sampleRate)
+{
+    QAudioFormat format;
+
+	/*
+	0x01 uLaw 1ch 8bit
+	0x02 PCM 1ch 8bit
+	0x04 PCM 1ch 16bit
+	0x08 PCM 2ch 8bit
+	0x10 PCM 2ch 16bit
+	0x20 uLaw 2ch 8bit
+	0x40 Opus 1ch
+	0x80 Opus 2ch
+	*/
+
+	format.setChannelCount(1);
+	format.setSampleSize(8);
+	format.setSampleType(QAudioFormat::UnSignedInt);
+	format.setByteOrder(QAudioFormat::LittleEndian);
+	format.setCodec("audio/pcm");
+
+	if (codec == 0x01 || codec == 0x20) {
+		/* Set sample to be what is expected by the encoder and the output of the decoder */
+		format.setSampleSize(16);
+		format.setSampleType(QAudioFormat::SignedInt);
+		format.setCodec("audio/PCMU");
+	}
+
+	if (codec == 0x08 || codec == 0x10 || codec == 0x20 || codec == 0x80) {
+		format.setChannelCount(2);
+	}
+
+	if (codec == 0x04 || codec == 0x10) {
+		format.setSampleSize(16);
+		format.setSampleType(QAudioFormat::SignedInt);
+	}
+
+	if (codec == 0x40 || codec == 0x80) {
+		format.setSampleSize(32);
+		format.setSampleType(QAudioFormat::Float);
+		format.setCodec("audio/opus");
+	}
+
+	format.setSampleRate(sampleRate);
+	return format;
+}
+
 #endif
